4944c020c46b8a3f448054b397df7b4341f3a250
admin <admin@admin>
2024-12-03 14:42:52 +0800

    icon

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1733204718',resources: [
+    cacheName: 'chirpy-1733207724',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
